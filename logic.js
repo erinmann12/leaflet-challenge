1b63170ef1ebfc87611c3f0efe9d21645f13ac5e
Erin Coates Mann <erin.mann2019@gmail.com>
2021-04-11 12:19:33 -0700

    successfully deployed map

--- logic.js
+++ logic.js
@@ -10,12 +10,14 @@ d3.json(queryUrl).then(function(data) {
   console.log(data.features);
 
 //   // Using the features array sent back in the API data, create a GeoJSON layer and add it to the map
-  function getColor(mag) {
+  function getColor(depth) {
 
-//     //Use if/else if etc on mag to change colors
-    if (mag > 3)
+//     //Use if/else if etc on depth to change colors
+    if (depth > 15)
       return "red"
-    else if (mag > 1.5)
+    else if (depth > 10)
+      return "orange"
+    else if (depth > 5)
       return "orange"
     else
       return "green";
@@ -35,7 +37,7 @@ d3.json(queryUrl).then(function(data) {
       return {
         "color": "white",
         "fillOpacity": 1,
-        "fillColor": getColor(feature.properties.mag),
+        "fillColor": getColor(feature.geometry.coordinates[2]),
         "weight": 5,
         // make circle proportionate to data
         "radius": feature.properties.mag * 5,
@@ -44,46 +46,46 @@ d3.json(queryUrl).then(function(data) {
     }  
   })
 
-//   // Define streetmap and darkmap layers
-//   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//     tileSize: 512,
-//     maxZoom: 18,
-//     zoomOffset: -1,
-//     id: "mapbox/streets-v11",
-//     accessToken: API_KEY
-//   });
+  // Define streetmap and darkmap layers
+  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  });
 
-//   var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//     maxZoom: 18,
-//     id: "dark-v10",
-//     accessToken: API_KEY
-//   });
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
 
-//   // Define a baseMaps object to hold our base layers
-//   var baseMaps = {
-//     "Street Map": streetmap,
-//     "Dark Map": darkmap
-//   };
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+  };
 
-//   var overlayMap = {
-//     "Earthquakes": earthquakes
-//   }
-//   // Create a new map
-//   var myMap = L.map("map", {
-//     center: [
-//       37.09, -95.71
-//     ],
-//     zoom: 5,
-//     layers: [streetmap, earthquakes]
-//   });
+  var overlayMap = {
+    "Earthquakes": earthquakes
+  }
+  // Create a new map
+  var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [streetmap, earthquakes]
+  });
 
-//   // Create a layer control containing our baseMaps
-//   // Be sure to add an overlay Layer containing the earthquake GeoJSON
-//   L.control.layers(baseMaps, overlayMap, {
-//     collapsed: false
-//   }).addTo(myMap);
+  // Create a layer control containing our baseMaps
+  // Be sure to add an overlay Layer containing the earthquake GeoJSON
+  L.control.layers(baseMaps, overlayMap, {
+    collapsed: false
+  }).addTo(myMap);
 
 //   //LEGEND GOES HERE
 
